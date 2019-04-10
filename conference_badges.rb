2c796fe92d56150faedc50c1c53ca86c59aa6992
Janice Pena <jipena94@gmail.com>
2019-04-10 02:32:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,19 +11,9 @@ def batch_badge_creator(array)
   end
 end
 
-def assign_rooms(array1)
-	array1.each_with_index.map do |name, index| 
+def assign_rooms(array)
+	array.each_with_index.map do |name, index| 
 		"Hello #{name}! You'll be assigned to room #{index + 1}"
 	end
 end
 
-def printer(guests)
-  
-  batch_badge_creator(guests).each do |badge|
-    puts badge
-  end
-
-  assign_rooms(guests).each do |room|
-    puts room
-    end
-end
