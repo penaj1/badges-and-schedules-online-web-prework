87d3a3fc942bd222cf13bcc7e26cccb54cacef8b
Janice Pena <jipena94@gmail.com>
2019-04-10 02:41:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,9 +12,6 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  room = 0 
-  attendees.collect do |name|
-    room += 1
 		"Hello #{name}! You'll be assigned to room #{room + 1}"
 	end
 end
