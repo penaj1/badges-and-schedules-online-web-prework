b57b723425ba31b7ee2a04760349ef0049617630
Janice Pena <jipena94@gmail.com>
2019-04-10 02:37:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,4 +20,11 @@ def assign_rooms(attendees)
 end
 
 def printer
+  batch_badge_creator(attendees).each do |value|
+    puts value
+  end
+  assign_rooms(attendees).each do |value|
+    puts value
+  end
+end
 
