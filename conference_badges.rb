02fdc5b8247553a1bf09536a26da32b6fcf6f818
Janice Pena <jipena94@gmail.com>
2019-04-10 02:40:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,11 +11,11 @@ def batch_badge_creator(attendees)
   end
 end
 
-def room_assignments(attendees)
-  index = 0 
+def assign_rooms(attendees)
+  room = 0 
   attendees.collect do |name|
-    index += 1
-		"Hello #{name}! You'll be assigned to room #{index + 1}"
+    room += 1
+		"Hello #{name}! You'll be assigned to room #{room + 1}"
 	end
 end
 
